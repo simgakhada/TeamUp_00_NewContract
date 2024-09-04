a271df5dec9825166a94367069dbd3b4047f9466
RRRR <msgpigeon@gmail.com>
2024-09-04 11:59:22 +0900

    Seperate from Group

--- src/main/java/simgakhada/teamup00/group/GroupDAO.java
+++ src/main/java/simgakhada/teamup00/group/GroupDAO.java
@@ -26,14 +26,12 @@ public class GroupDAO
 
     public void print(Connection con)
     {
-        Properties prop = new Properties();
         System.out.println("[그룹 출력] 현재 등록된 그룹을 모두 출력합니다.");
         System.out.println("출력 결과");
         Statement stmt;
         ResultSet rs;
 
         try {
-            prop.loadFromXML(new FileInputStream("src/main/resources/mapper/menu-query.xml"));
             String query = prop.getProperty("printGroup");
             stmt = con.createStatement();
             rs = stmt.executeQuery(query);
@@ -45,7 +43,7 @@ public class GroupDAO
                 while(rs.next())
                     System.out.println("그룹명: " + rs.getString("GROUP_NAME"));
             System.out.println();
-        } catch (SQLException | IOException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
@@ -84,18 +82,32 @@ public class GroupDAO
     {
         Scanner sc = new Scanner(System.in);
         PreparedStatement ps;
+        PreparedStatement ps2;
+        PreparedStatement ps3;
         String query = prop.getProperty("deleteGroup");
+        String query2 = prop.getProperty("deleteGroupForced");
         int result = 0;
 
         try {
+            ps2 = con.prepareStatement(query2);
             ps = con.prepareStatement(query);
             System.out.println("[그룹 삭제] 그룹을 삭제합니다.");
+            System.out.println("연락처가 존재하는 그룹을 삭제할 경우, 그룹 내의 연락처는 그룹 미지정으로 이동됩니다.");
             System.out.print("삭제하실 그룹명을 입력해주세요.: ");
             String groupDelete = sc.nextLine();
             System.out.println();
 
+            ps3 = con.prepareStatement("SET foreign_key_checks = 0");
+            ps3.executeUpdate();
+
+            ps2.setString(1, groupDelete);
+            ps2.executeUpdate();
+
             ps.setString(1, groupDelete);
             result = ps.executeUpdate();
+
+            ps3 = con.prepareStatement("SET foreign_key_checks = 1");
+            ps3.executeUpdate();
 
             if (result == 1) {
                 System.out.println("입력하신 그룹이 성공적으로 삭제되었습니다.");
